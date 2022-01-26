3431e6c510865e844118d80226aa006e93ec098c
Douglas Soares <douglasmaismcpe@gmail.com>
2022-01-26 19:47:56 -0300

    Configuring WebSocket in the application

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 
-app.listen(5500, () => console.log("rodando"));
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("Socket: ", socket.id);
+});
+
+server.listen(5500, () => console.log("rodando"));
