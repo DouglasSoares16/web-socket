89fc7ea286e9cf5492e851ca48c75e610ec40c06
Douglas Soares <douglasmaismcpe@gmail.com>
2022-01-27 12:17:33 -0300

    Created Users Table

---
+++ src/database/migrations/1643294681623-Create-User-Table.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUserTable1643294681623 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "users",
+            columns: [
+                {
+                    name: "id",
+                    type: "uuid",
+                    isPrimary: true
+                },
+                {
+                    name: "name",
+                    type: "varchar"
+                },
+                {
+                    name: "email",
+                    type: "varchar"
+                },
+                {
+                    name: "avatar",
+                    type: "varchar"
+                },
+                {
+                    name: "socket_id",
+                    type: "varchar"
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "now()"
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
