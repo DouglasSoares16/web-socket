47da079b605ffb4d4f865a96d9e0394a12ac2995
Douglas Soares <douglasmaismcpe@gmail.com>
2022-01-29 11:51:34 -0300

    Created Use Case of List All User

---
+++ src/modules/users/useCases/listAllUser/ListAllUserUseCase.ts
@@ -0,0 +1,19 @@
+import { inject, injectable } from "tsyringe";
+import { User } from "../../infra/typeorm/entities/User";
+import { IUserRepository } from "../../repositories/IUserRepository";
+
+@injectable()
+class ListAllUserUseCase {
+  constructor(
+    @inject("UserRepository")
+    private userRepository: IUserRepository
+  ) {}
+
+  async execute(): Promise<User[]> {
+    const users = await this.userRepository.findAll();
+
+    return users;
+  }
+}
+
+export { ListAllUserUseCase };
