f0f6d4c47056571681da7393d5377dab138cd99f
Douglas Soares <douglasmaismcpe@gmail.com>
2022-01-29 11:53:46 -0300

    Inserting users on the screen

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,11 +7,58 @@ function onLoad() {
   const avatar = urlParams.get("avatar");
   const email = urlParams.get("email");
 
+  document.querySelector(".user_logged").innerHTML += `
+    <img
+      class="avatar_user_logged"
+      src="${avatar}"
+    />
+    <strong id="user_logged">${name}</strong>
+  `;
+
+  // Evento do Start
   socket.emit("start", {
     email,
     name,
     avatar
   });
-} 
+
+  /* Evento que ficará escutando se há novos usuários, e se há novos usuários quero que ele atualize mostrando na tela */
+  socket.on("new_users", (data) => {
+    const existInDiv = document.getElementById(`user_${data.id}`);
+
+    if (!existInDiv) {
+      addUsers(data);
+    }
+  });
+
+  /* Ao usuário se conectar, listar todos os usuários */
+  socket.emit("get_users", (users) => {
+    console.log("get users:  ", users);
+
+    users.map((user) => {
+      if (user.email !== email) {
+        addUsers(user);
+      }
+    });
+  });
+}
+
+function addUsers(user) {
+  const userList = document.getElementById("users_list");
+
+  userList.innerHTML += `
+    <li
+      class="user_name_list"
+      id="user_${user.id}"
+      idUser="${user.id}"
+    >
+      <img
+        class="nav_avatar"
+        src=${user.avatar}
+      />
+      ${user.name}
+    </li>
+  `;
+}
 
 onLoad();
