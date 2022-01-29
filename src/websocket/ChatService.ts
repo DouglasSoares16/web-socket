a32cba13c67f315097d7823aaea85d0f3f35de4a
Douglas Soares <douglasmaismcpe@gmail.com>
2022-01-29 09:53:55 -0300

    Listener the Start Event in Chat Service

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,7 +1,28 @@
+import { container } from "tsyringe";
 import { io } from "../http";
+import { CreateUserUseCase } from "../modules/users/useCases/createUser/CreateUserUseCase";
+import { UserRepository } from "../modules/users/infra/typeorm/implementations/UserRepository";
 
 io.on("connect", (socket) => {
-  socket.emit("chat_init", {
-    message: "Seu chat foi iniciado",
-  })
+  socket.on("start", async (data) => {
+    const {
+      name,
+      email,
+      avatar
+    } = data;
+
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+    const userRepository = new UserRepository();
+
+    await createUserUseCase.execute({
+      name,
+      email,
+      avatar,
+      socket_id: socket.id,
+    });
+
+    const user = await userRepository.findByEmail(email);
+
+    console.log(user);
+  });
 });
