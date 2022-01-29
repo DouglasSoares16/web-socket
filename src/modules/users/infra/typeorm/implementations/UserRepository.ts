0999ac0c6dbae8f216284f28928cb4decdf59e1a
Douglas Soares <douglasmaismcpe@gmail.com>
2022-01-29 09:51:23 -0300

    Created 'findByEmail' Method to User Repository

--- src/modules/users/infra/typeorm/implementations/UserRepository.ts
+++ src/modules/users/infra/typeorm/implementations/UserRepository.ts
@@ -10,6 +10,16 @@ class UserRepository implements IUserRepository {
     this.repository = getRepository(User);
   }
 
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({
+      where: {
+        email
+      }
+    });
+
+    return user;
+  }
+
   async create(data: ICreateUserDTO): Promise<void> {
     const user = this.repository.create(data);
 
--- src/modules/users/repositories/IUserRepository.ts
+++ src/modules/users/repositories/IUserRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../infra/typeorm/entities/User";
 
 interface IUserRepository {
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUserRepository };
