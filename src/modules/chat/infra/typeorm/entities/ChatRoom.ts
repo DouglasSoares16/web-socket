ca465cb8105ff09d6780d6770418e4d89ec9831a
Douglas Soares <douglasmaismcpe@gmail.com>
2022-01-29 09:50:04 -0300

    Created Entity of ChatRoom

---
+++ src/modules/chat/infra/typeorm/entities/ChatRoom.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+import { User } from "../../../../users/infra/typeorm/entities/User";
+
+@Entity("chat_room")
+class ChatRoom {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  users_id: string;
+
+  @ManyToMany(() => User)
+  @JoinTable()
+  users: User[];
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { ChatRoom };
