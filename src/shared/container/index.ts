1e82e01089eb92c78f3c823be258f5529dfd5641
Douglas Soares <douglasmaismcpe@gmail.com>
2022-02-01 10:11:13 -0300

    Added ChatRoom Repository to Container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe";
+import { ChatRoomRepository } from "../../modules/chat/infra/typeorm/implementations/ChatRoomRepository";
+import { IChatRoomRepository } from "../../modules/chat/repositories/IChatRoomRepository";
 import { UserRepository } from "../../modules/users/infra/typeorm/implementations/UserRepository";
 import { IUserRepository } from "../../modules/users/repositories/IUserRepository";
 
 container.registerSingleton<IUserRepository>(
   "UserRepository",
   UserRepository
+);
+
+container.registerSingleton<IChatRoomRepository>(
+  "ChatRoomRepository",
+  ChatRoomRepository
 );
