53865df425b0e591c7bd1c7e7a1ebea5a9fa6a94
Douglas Soares <douglasmaismcpe@gmail.com>
2022-02-05 11:00:38 -0300

    Cleaning the 'message_user' div

--- public/js/chat.js
+++ public/js/chat.js
@@ -47,6 +47,8 @@ function onLoad() {
 }
 
 document.getElementById("users_list").addEventListener("click", (event) => {
+  document.getElementById("message_user").innerHTML = "";
+
   if (event.target && event.target.matches("li.user_name_list")) {
     const idUser = event.target.getAttribute("idUser");
 
